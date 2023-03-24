81d3f5500d19d329b2e601b4e652aadeec3ce5d5
shawon <shawon.softeng@gmail.com>
2023-03-24 18:53:51 +0600

    commit

--- my-app/src/pages/About.jsx
+++ my-app/src/pages/About.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Layout from "../components/layout/Layout";
 
 const About = () => {
+    let count = 1;
     return (
         <div>
             <Layout>
                 <h1> Hello I am about page</h1>
+                <h2> Count: {count} </h2>
             </Layout>
         </div>
     );
